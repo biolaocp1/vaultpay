a16b82c99f4145b66b7496067fcea11b3108a285
biolaocp1 <olaibiabiola@gmail.com>
2026-05-26 11:31:58 +0200

    feat: fix compiler version, all 5 tests passing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
